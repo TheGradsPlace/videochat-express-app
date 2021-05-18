6c0717a2342b22a1fba11d1a4796a40ba0baf56e
bryanronnie <bryanronniej18023@it.ssn.edu.in>
2021-05-18 14:26:12 +0530

    end meeting

--- public/script.js
+++ public/script.js
@@ -152,13 +152,6 @@ const closeChat = () => {
 }
 
 
-app.get('/hello', function(req, res){
-  res.send("Hello World!");
-});
-
-app.post('/hello', function(req, res){
-  res.send("You just called the post method at '/hello'!\n");
-});
 
 
 
--- server.js
+++ server.js
@@ -22,11 +22,6 @@ app.get('/', (req, res) => {
 
 
 
-
-
-
-
-
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
